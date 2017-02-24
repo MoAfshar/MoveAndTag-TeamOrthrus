64e77a70becade0d5895c65247aa0dabf226c9bc
Brian Min <bymi15@yahoo.com>
2017-02-24 13:49:20 +0000

    added cluster branch

--- clusterTag/src/Algorithm.java
+++ clusterTag/src/Algorithm.java
@@ -1,4 +1,8 @@
+package MoveAndTagRobots;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.PriorityQueue;
 
 public class Algorithm {
@@ -9,19 +13,17 @@ public class Algorithm {
 
 	private Point[] allPoints;
 	private boolean allFound = false;
-	private static final double MAXVAL = Double.MAX_VALUE / 2 - 1;
+	public static final double MAXVAL = Double.MAX_VALUE / 2 - 1;
 	private ArrayList<ArrayList<Point>> allPaths = new ArrayList<>();
 
 	private int totalRobots;
-	private int robotsFound = 1;
+	private int robotsFound;
+	
+	PriorityQueue<RobotEdge> edges;
 
-	private double calculateDistance(Point p1, Point p2) {
+	public static double calculateDistance(Point p1, Point p2) {
 		return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y)
 				* (p2.y - p1.y));
-	}
-
-	public void algorithmStart() {
-
 	}
 
 	public double findY(Point p1, Point p2, double new_x) {
@@ -120,11 +122,11 @@ public class Algorithm {
 	}
 
 	public Intersection isIntersecting(Point p1, Point p2, Point p3, Point p4) {
-		if (p1.isEqual(p3) || p1.isEqual(p4)) {
+		if (p1.equals(p3) || p1.equals(p4)) {
 			return Intersection.P1SAME;
 		}
 
-		if (p2.isEqual(p3) || p2.isEqual(p4)) {
+		if (p2.equals(p3) || p2.equals(p4)) {
 			return Intersection.P2SAME;
 		}
 
@@ -136,7 +138,8 @@ public class Algorithm {
 		return Intersection.NO;
 	}
 
-	public void calculatePointVisibility(Point[] allPoints, ArrayList<Point[]> obstaclePoints) {
+	public void calculatePointVisibility(Point[] allPoints,
+			ArrayList<Point[]> obstaclePoints) {
 		int len = allPoints.length;
 		ArrayList<Point> visiblePoints = new ArrayList<>();
 		boolean collides = false;
@@ -144,7 +147,8 @@ public class Algorithm {
 			for (int j = 0; j < len; j++) {
 				if (j != i) {
 					for (Point[] obstaclePoint : obstaclePoints) {
-						if (pathCollidesObstacle(allPoints[i], allPoints[j], obstaclePoint)) {
+						if (pathCollidesObstacle(allPoints[i], allPoints[j],
+								obstaclePoint)) {
 							collides = true;
 							break;
 						}
@@ -159,10 +163,8 @@ public class Algorithm {
 		}
 	}
 
-	public void init(Point[] allPoints, ArrayList<Point[]> obstaclePoints, int pointNum, int mapNum) {
-		calculatePointVisibility(allPoints, obstaclePoints);
-
-		this.allPoints = new Point[pointNum];
+	public void init(int pointNum) {
+		this.allPoints = new Point[pointNum]; 
 
 		floydTable = new double[pointNum][pointNum];
 		warshalls = new int[pointNum][pointNum];
@@ -171,9 +173,6 @@ public class Algorithm {
 				warshalls[i][j] = -1;
 			}
 		}
-
-		calcFloydDistances(allPoints);
-		calcFloydPaths(allPoints);
 	}
 
 	public void calcFloydDistances(Point[] points) {
@@ -206,21 +205,16 @@ public class Algorithm {
 		}
 	}
 
-
-
-
-	/*
-	FLOYD WARSHALL STUFF ALL ABOVE, IGNORE WHAT'S BELOW HERE
-	 */
-
 	public String path(int x, int y, Point[] points) {
 		int k = warshalls[x][y];
 		if (k == -1)
-			return "";
+			return "";// points[x].toString() + " cannot be reached from " +
+						// points[y].toString();
+			// else System.out.println("K IS " + k);
 		if (k == -2)
 			return points[y].toString() + ", ";
 		if (k == -3)
-			return "";
+			return "";// "same point!";
 		return path(x, k, points) + path(k, y, points);
 	}
 
@@ -395,31 +389,29 @@ public class Algorithm {
 	public String callBuildTree(Point[] points, int numRobots) {
 		allPoints = points;
 		totalRobots = numRobots;
-		PriorityQueue<Element> elements = new PriorityQueue<Element>() {
-			public int compare(Element o1, Element o2) {
+		PriorityQueue<RobotEdge> edges = new PriorityQueue<RobotEdge>() {
+			public int compare(RobotEdge o1, RobotEdge o2) {
 				return (int) (o1.totalDistance - o2.totalDistance);
 			}
 		};
 		double[] nearestArray = findNearestPoint(0);
 		int nearest = (int) nearestArray[0];
 		double dist = nearestArray[1];
-		Element first_elem = new Element(0, nearest, dist);
-		elements.add(first_elem);
-		ArrayList<Element> savedElements = new ArrayList<>();
+		RobotEdge first_elem = new RobotEdge(0, nearest, dist);
+		edges.add(first_elem);
+		ArrayList<RobotEdge> savedEdges = new ArrayList<>();
 
-		while (!elements.isEmpty()) {
-			// if (savedElements.size()==numRobots) break;
-			// System.out.println("");
-			Element current = elements.poll();
-			savedElements.add(current);
+		while (!edges.isEmpty()) {
+			RobotEdge current = edges.poll();
+			savedEdges.add(current);
 			if (robotsFound == totalRobots) {
 				continue;
 			}
-			double[] nearest1 = findNearestPoint(current.robot);
+			double[] nearest1 = findNearestPoint(current.source);
 			int nearest1_index = (int) nearest1[0];
 			if (nearest1_index != current.destination) {
 				double nearest1_dist = nearest1[1];
-				elements.add(new Element(current.robot, nearest1_index,
+				edges.add(new RobotEdge(current.source, nearest1_index,
 						current.totalDistance + nearest1_dist));
 			}
 			robotsFound++;
@@ -430,20 +422,20 @@ public class Algorithm {
 			int nearest2_index = (int) nearest2[0];
 			if (nearest2_index != current.destination) {
 				double nearest2_dist = nearest2[1];
-				elements.add(new Element(current.destination, nearest2_index,
+				edges.add(new RobotEdge(current.destination, nearest2_index,
 						current.totalDistance + nearest2_dist));
 			}
 			robotsFound++;
 		}
-		ArrayList<ArrayList<Integer>> chosenPaths = new ArrayList<ArrayList<Integer>>(
+		ArrayList<ArrayList<Integer>> chosenPaths = new ArrayList<>(
 				totalRobots);
 		for (int i = 0; i < totalRobots; i++) {
 			ArrayList<Integer> chosenPath = new ArrayList<>();
 			chosenPath.add(i);
-			int elemSize = savedElements.size();
+			int elemSize = savedEdges.size();
 			for (int j = 0; j < elemSize; j++) {
-				Element elem = savedElements.get(j);
-				if (elem.robot == i) {
+				RobotEdge elem = savedEdges.get(j);
+				if (elem.source == i) {
 					chosenPath.add(elem.destination);
 				}
 			}
@@ -466,21 +458,341 @@ public class Algorithm {
 	public String path_new(int x, int y) {
 		int k = warshalls[x][y];
 		if (k == -1)
-			return "";
+			return "";// globalPoints[x].toString() + " cannot be reached from "
+						// + globalPoints[y].toString();
+			// else System.out.println("K IS " + k);
 		if (k == -2)
 			return allPoints[y].toString() + ", ";
 		if (k == -3)
-			return "";
+			return "";// "same point!";
 		return path_new(x, k) + path_new(k, y);
 	}
 
 	public String getRobotString(ArrayList<Integer> pointsIndex) {
-		int len = pointsIndex.size();
 		String output = allPoints[pointsIndex.get(0)].toString() + ", ";
-		for (int i = 1; i < len; i++) {
+		
+		for (int i = 1; i < pointsIndex.size(); i++) {
 			output = output
 					+ path_new(pointsIndex.get(i - 1), pointsIndex.get(i));
 		}
 		return output.substring(0, output.length() - 2);
+	}
+	
+	public String getRobotString(ArrayList<Integer> pointsIndex, Point[] robotPoints) {
+		String output = findRobotByIndex(pointsIndex.get(0), robotPoints).toString() + ", ";
+		
+		for (int i = 1; i < pointsIndex.size(); i++) {
+			output = output
+					+ path_new(pointsIndex.get(i - 1), pointsIndex.get(i));
+		}
+		return output.substring(0, output.length() - 2);
+	}
+
+	public String runClusterAlgorithm(int k, Point[] points, Point[] robotPoints){ 
+		//chosenPaths = new ArrayList<>(robotPoints.length);
+		allPoints = points;
+
+		edges = new PriorityQueue<RobotEdge>() {
+			public int compare(RobotEdge o1, RobotEdge o2) {
+				return (int) (o1.totalDistance - o2.totalDistance);
+			}
+		};
+		
+		HashMap<Integer, Cluster> clusters = generateClusters(k, robotPoints, floydTable);
+		Cluster firstCluster = null; //contains start robot
+		for(Map.Entry<Integer, Cluster> entry : clusters.entrySet()){
+			Cluster c = entry.getValue();
+			if(c.containsFirstRobot()){
+				firstCluster = c;
+				break;
+			}
+		}
+
+		//find the first robot in the first cluster
+		Point[] robots = firstCluster.getRobotPoints();
+		Point firstRobot = null;
+		for(int i = 0; i < robots.length; i++){
+			if(robots[i].index == 0){
+				firstRobot = robots[i];
+			}
+		}
+
+		//wake up robots in all clusters starting with the first
+		return wakeUpRobotsInCluster(firstRobot, points, robotPoints.length, clusters);
+
+	}
+
+	public String wakeUpRobotsInCluster(Point firstRobot, Point[] allPoints, int numRobots, HashMap<Integer, Cluster> allClusters){
+
+		int firstRobotClusterID = firstRobot.cluster;
+		Cluster firstCluster = allClusters.get(firstRobotClusterID);
+		firstCluster.robotsWoken = 1;
+		firstCluster.claimed = true;
+		
+		//first try waking up the robot at the centre of the cluster
+		if(!firstCluster.centreOfMassPoint.hasWoken){
+			int nearest = firstCluster.centreOfMassPoint.index;
+			double dist = floydTable[firstRobot.index][nearest];
+			RobotEdge first_elem = new RobotEdge(firstRobotClusterID, firstRobotClusterID, firstRobot.index, nearest, dist);
+			edges.add(first_elem);
+		}else{
+			double[] nearestArray = findNearestPointInCluster(firstRobot.index, firstCluster);
+			
+			if(nearestArray != null){
+				int nearest = (int) nearestArray[0];
+				double dist = nearestArray[1];
+				RobotEdge first_elem = new RobotEdge(firstRobotClusterID, firstRobotClusterID, firstRobot.index, nearest, dist);
+				edges.add(first_elem);
+			}
+		}
+
+		ArrayList<RobotEdge> savedEdges = new ArrayList<>();
+
+		while (!edges.isEmpty()) {
+			RobotEdge current = edges.poll();
+			allClusters.get(current.sourceClusterID).totalRobots = allClusters.get(current.sourceClusterID).robots.size();
+			allClusters.get(current.sourceClusterID).claimed = true;
+			allClusters.get(current.sourceClusterID).robotsWoken++;
+			
+			savedEdges.add(current);
+			
+			if (allClusters.get(current.sourceClusterID).robotsWoken == allClusters.get(current.sourceClusterID).totalRobots) {
+				continue;
+			}
+
+			double[] nearest1 = findNearestPointInCluster(current.source, allClusters.get(current.sourceClusterID));
+			double[] nearest2 = findNearestPointInCluster(current.destination, allClusters.get(current.destinationClusterID));
+			
+			if(nearest1 == null){
+				Point robotPoint = findRobotByIndex(current.source, allClusters.get(current.sourceClusterID).getRobotPoints());
+				Cluster nextClosestCluster = getNearestCluster(robotPoint, allClusters);
+				Point clusterCenter = nextClosestCluster.centreOfMassPoint;
+				if(!clusterCenter.hasWoken){
+					edges.add(new RobotEdge(current.sourceClusterID, nextClosestCluster.clusterID, current.source, clusterCenter.index,
+						current.totalDistance + floydTable[current.source][clusterCenter.index]));
+				}else{
+					double[] nearestInCluster = findNearestPointInCluster(current.source, nextClosestCluster);
+					int nearestC_index = (int) nearestInCluster[0];
+					edges.add(new RobotEdge(current.sourceClusterID, nextClosestCluster.clusterID, current.source, nearestC_index,
+							current.totalDistance + floydTable[current.source][nearestC_index]));
+				}
+			}else {
+				int nearest1_index = (int) nearest1[0];
+				if (nearest1_index != current.destination) {
+					double nearest1_dist = nearest1[1];
+					edges.add(new RobotEdge(current.sourceClusterID, current.sourceClusterID, current.source, nearest1_index,
+							current.totalDistance + nearest1_dist));
+				}
+			}
+			if(nearest2 == null){
+				Point robotPoint = findRobotByIndex(current.destination, allClusters.get(current.destinationClusterID).getRobotPoints());
+				Cluster nextClosestCluster = getNearestCluster(robotPoint, allClusters);
+				Point clusterCenter = nextClosestCluster.centreOfMassPoint;
+				if(!clusterCenter.hasWoken){
+					edges.add(new RobotEdge(current.sourceClusterID, nextClosestCluster.clusterID, current.destination, clusterCenter.index,
+						current.totalDistance + floydTable[current.destination][clusterCenter.index]));
+				}else{
+					double[] nearestInCluster = findNearestPointInCluster(current.destination, nextClosestCluster);
+					int nearestC_index = (int) nearestInCluster[0];
+					edges.add(new RobotEdge(current.destinationClusterID, nextClosestCluster.clusterID, current.destination, nearestC_index,
+							current.totalDistance + floydTable[current.destination][nearestC_index]));
+				}
+			}else {
+				int nearest2_index = (int) nearest2[0];
+				if (nearest2_index != current.destination) {
+					double nearest2_dist = nearest2[1];
+					edges.add(new RobotEdge(current.destinationClusterID, current.destinationClusterID, current.destination, nearest2_index,
+							current.totalDistance + nearest2_dist));
+				}
+			}
+		}
+		
+		ArrayList<ArrayList<Integer>> chosenPaths = new ArrayList<>(numRobots);
+		for (int i = 0; i < numRobots; i++) {
+			ArrayList<Integer> chosenPath = new ArrayList<>();
+			chosenPath.add(i);
+			int elemSize = savedEdges.size();
+			for (int j = 0; j < elemSize; j++) {
+				RobotEdge elem = savedEdges.get(j);
+				if (elem.source == i) {
+					chosenPath.add(elem.destination);
+				}
+			}
+			if (chosenPath.size() > 1)
+				chosenPaths.add(chosenPath);
+		}
+
+		String result = "";
+
+		for (ArrayList<Integer> path : chosenPaths) {
+			if (path == null || path.size() == 0)
+				continue;
+			String current_path = getRobotString(path);
+			result += current_path + "; ";
+		}
+
+		//System.out.println(result.substring(0, result.length() - 2));
+		return result.substring(0, result.length() - 2);
+	}
+	
+	Point findRobotByIndex(int index, Point[] robots){
+		for(Point robot : robots){
+			if(robot.index == index){
+				return robot;
+			}
+		}
+		return null;
+	}
+
+	public double[] findNearestPointInCluster(int current, Cluster cluster) {
+		if(cluster.robotsWoken == cluster.totalRobots) return null;
+
+		Point[] robotsInCluster = cluster.getRobotPoints();
+		double[] res = new double[2];
+		double minDist = MAXVAL;
+		int minIndex = robotsInCluster[0].index;
+		int minI = 0;
+		
+		boolean allAwake = true;
+
+		for (int i = 0; i < robotsInCluster.length; i++) {
+			if (!robotsInCluster[i].hasWoken) {
+				allAwake = false;
+				if (minDist > floydTable[current][i]) {
+					minDist = floydTable[current][i];
+					minIndex = robotsInCluster[i].index;
+					minI = i;
+				}
+			}
+		}
+		
+		if(allAwake){
+			return null;
+		}
+		
+		robotsInCluster[minI].hasWoken = true;
+		res[0] = minIndex;
+		res[1] = minDist;
+		return res;
+	}
+
+	public Cluster getNearestCluster(Point p1, HashMap<Integer, Cluster> clusters){
+		double minDist = MAXVAL;
+		Cluster minCluster = null;
+
+		int claimedCount = 0;
+
+		for(Map.Entry<Integer, Cluster> entry : clusters.entrySet()){
+			Cluster c = entry.getValue();
+			if(c.claimed){
+				claimedCount++;
+				continue;
+			}
+
+			Point p2 = c.centreOfMassPoint;
+			if(p2.equals(p1)){
+				continue;
+			}
+
+			if(minDist > floydTable[p1.index][p2.index]){
+				minDist = floydTable[p1.index][p2.index];
+				minCluster = c;
+			}
+		}
+
+		//if all clusters are claimed - return null
+		if(claimedCount == clusters.size()){
+			//System.out.println("ALL CLAIMED");
+			return null;
+		}
+
+		return minCluster;
+	}
+
+	public HashMap<Integer, Cluster> generateClusters(int k, Point[] robotPoints, double[][] floydTable){
+		if(k > robotPoints.length) k = robotPoints.length;
+
+		HashMap<Integer, Cluster> clusters = new HashMap<>();
+		//initialise clusters (each robot is a cluster of size 1 initially)
+		for(int i = 0; i < robotPoints.length; i++){
+			robotPoints[i].cluster = i;
+			robotPoints[i].index = i;
+
+			Cluster c = new Cluster(i);
+			c.robots.add(robotPoints[i]);
+			clusters.put(i, c);
+		}
+
+		double[][] floydCopy = new double[robotPoints.length][robotPoints.length];
+		System.arraycopy(floydTable, 0, floydCopy, 0, robotPoints.length);
+
+		int numClusters = clusters.size();
+
+		double minDist = MAXVAL;
+		int minIndexA = 0;
+		int minIndexB = 0;
+
+		while(numClusters > k) {
+			minDist = MAXVAL;
+			for (int i = 0; i < robotPoints.length; i++) {
+				for (int j = 0; j < robotPoints.length; j++) {
+					if (floydCopy[i][j] < minDist && i!=j) {
+						minDist = floydCopy[i][j];
+						minIndexA = i;
+						minIndexB = j;
+					}
+				}
+			}
+			floydCopy[minIndexA][minIndexB] = MAXVAL;
+			floydCopy[minIndexB][minIndexA] = MAXVAL;
+
+			int clusterIndexA = robotPoints[minIndexA].cluster;
+			int clusterIndexB = robotPoints[minIndexB].cluster;
+
+			Cluster a = clusters.get(clusterIndexA);
+			Cluster b = clusters.get(clusterIndexB);
+
+			//set edges in the same cluster to MAXVAL so it doesn't check again
+			for(Point pointA : a.robots){
+				for(Point pointB : b.robots){
+					floydCopy[pointA.index][pointB.index] = MAXVAL;
+					floydCopy[pointB.index][pointA.index] = MAXVAL;
+				}
+			}
+
+			if(a==null || b==null){
+				System.out.println("cluster not found " + clusterIndexA + " " + clusterIndexB);
+				continue;
+			}
+
+			for(Point p : b.robots){
+				//modifies the cluster index for each robot in the union cluster
+				robotPoints[p.index].cluster = clusterIndexA;
+			}
+			a.union(b); //merges b into a
+			clusters.remove(clusterIndexB);
+			numClusters--;
+		}
+
+		for(Map.Entry<Integer, Cluster> entry : clusters.entrySet()){
+			Cluster cluster = entry.getValue();
+
+			cluster.calculateCentre();
+/*
+			System.out.println("Cluster " + cluster.clusterID + " (size: " + cluster.robots.size() + ")");
+			for(Point robot : cluster.robots) {
+				System.out.print(robot.toString() + " ");
+			}
+			System.out.println("Centre point: " + cluster.centreOfMassPoint);
+			System.out.println("");*/
+		}
+		/*
+		ArrayList<Cluster> finalOutput = new ArrayList<>();
+		for(Map.Entry<Integer, Cluster> entry : clusters.entrySet()) {
+			Cluster cluster = entry.getValue();
+			cluster.calculateCentre();
+			finalOutput.add(cluster);
+		}*/
+		return clusters;
 	}
 }
